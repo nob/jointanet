fac7d6723f0f006ae789f47f509bfcf84c4c6378
nob <yanbe.n@jointanet.com>
2018-08-07 18:34:17 +0900

    404 page.

---
+++ pages/_locale/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="details">
+      <div class="container">
+          <div class="row">
+  	        <div class="span8 offset2">
+              <h1 class="text-center">{{ title }}<small><br>{{ subtitle }}</small></h1>
+              <div v-html="bodyHtml"></div>
+  	        </div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import { sourceFileArray, fileMap } from '../../../contents/json/summary.json';
+export default {
+  validate ({ params, store }) {
+    return sourceFileArray.includes(`contents/404.${params.locale}.md`);
+  },
+  asyncData ({ params, store}) {
+    return Object.assign({}, require(`~/contents/json/404.${params.locale}.json`), { params });
+  },
+};
+</script>
+
+<style>
+</style>
