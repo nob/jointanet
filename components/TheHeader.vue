05a1a25883fc433c3bcf72cc48593898118dc4b5
nob <yanbe.n@jointanet.com>
2018-08-08 10:35:57 +0900

    ページヘッダ: リンク先 & 戻るボタン表示・非表示　修正

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -5,11 +5,11 @@
       <div class="container clearfix">
           <a class="logo" :href="`/${this.$route.params.locale}`"><img src="~/assets/img/logo-l.png" alt="Jointanet"></a>
           <ul class="nav visible-desktop clearfix">
-              <li class="text-center active"><a href="#services"><strong>{{ navi.sec1 }}</strong></a></li>
-              <li class="text-center active"><a href="#price"><strong>{{ navi.sec2 }}</strong></a></li>
-              <li class="text-center active"><a href="#works"><strong>{{ navi.sec3 }}</strong></a></li>
+              <li class="text-center"><a :href="`/${this.$route.params.locale}/#services`"><strong>{{ navi.sec1 }}</strong></a></li>
+              <li class="text-center"><a :href="`/${this.$route.params.locale}/#price`"><strong>{{ navi.sec2 }}</strong></a></li>
+              <li class="text-center"><a :href="`/${this.$route.params.locale}/#works`"><strong>{{ navi.sec3 }}</strong></a></li>
           </ul>
-          <a v-if="this.$route.path !== 'en'" id="btn-back-to-home" class="btn btn-inverse pull-left hidden-desktop" href="" type="button"><i class="icon-chevron-left icon-white"></i>&nbsp;戻る</a>
+          <a v-if="!(/(en|ja)\/*$/i.test(this.$route.path))" id="btn-back-to-home" class="btn btn-inverse pull-left hidden-desktop" href="" type="button"><i class="icon-chevron-left icon-white"></i>&nbsp;戻る</a>
       </div>
   </header>
 </template>
